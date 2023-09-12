fec1b66aa12b413dda9ee9584b0d0d4b6071af80
SinthiaGonzalez <gonzalez.sinthia@icloud.com>
2023-09-13 01:43:26 +0200

    paginacioncommit

--- client/src/views/homePage/home.jsx
+++ client/src/views/homePage/home.jsx
@@ -22,13 +22,18 @@ function Home() {
 
   //*paginado
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8; // Número de elementos por página
-  const indexOfLastItem = currentPage * itemsPerPage; // Índice del último elemento de cada página
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage; // Índice del primer elemento de cada página
+  const itemsPerPage = 8; // Número de elementos que se muestra por página
+  const indexOfLastItem = currentPage * itemsPerPage; // Índice del último elemento de cada página = a la pagina actual * el numero de elementos por pagina y eso nos devuelve el ultimo elemento de la pagina
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage; // Índice del primer elemento == el numero del ultimo elemento - el numero de elementos por pagina y eso nos devuelve el primer elemento de la pagina
 
-  const gamesToDisplay = selectedOrigin.length > 0 ? filteredByOrigin : filteredGames;
-  const gamesOnCurrentPage = gamesToDisplay.slice(indexOfFirstItem, indexOfLastItem);
+  const gamesToDisplay = selectedOrigin.length > 0 ? filteredByOrigin : filteredGames;// si hay un origen seleccionado, mostramos los juegos filtrados por origen, si no, mostramos los juegos filtrados por genero
+  const gamesOnCurrentPage = gamesToDisplay.slice(indexOfFirstItem, indexOfLastItem); // creamos una variable que va a contener los juegos que se van a mostrar en la pagina actual, le hace un slice  a gamestodisplay y le indica los valores de los indices que va a mostrar que estan representados por las variables indexOfFirstItem y indexOfLastItem
 
+//total de paginas que se van a mostrar
+const totalPages = Math.ceil(gamesToDisplay.length / itemsPerPage)
+
+
+  // creamos el handle next 
   const handleNextPage = () => {
     if (currentPage < Math.ceil(gamesToDisplay.length / itemsPerPage)) {
       setCurrentPage(currentPage + 1);
@@ -53,6 +58,7 @@ function Home() {
       <Filtros />
       <div>
         <button onClick={handlePrevPage} className='btn'>prev</button>
+        <span className='span'> {currentPage} de {totalPages}</span>
         <button onClick={handleNextPage} className='btn'>next</button>
       </div>
       <Cards Allgames={gamesOnCurrentPage} />
